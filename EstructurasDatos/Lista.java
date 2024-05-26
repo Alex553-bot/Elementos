a90c8c8f0fbe798b39cc5a92c469e1b201a444aa
Alex Alvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-05-26 19:09:47 -0400

    problem: solving

--- EstructurasDatos/Lista.java
+++ EstructurasDatos/Lista.java
@@ -20,7 +20,26 @@ public class Lista<T extends Comparable>
 		if (it==null) return null;
 		return it; 
 	}
-	public void add(T nuevo, int i) {
+	public void add(T nuevo) {
+		Nodo<T> x = new Nodo<T>(nuevo); 
+
+		Nodo<T> it = root; 
+		while (it.next!=root && it.dato.compareTo(x.dato) < 1)
+			it = it.next;
+		if (it.next==root && it.dato.compareTo(x.dato) < 1) {
+			it.next = x; 
+			x.prev = it; 
+			x.next = root; 
+			root.prev = x; 
+		}  //[1, 2, 3, 4], 5
+		else {
+			x.prev = it.prev; 
+			it.prev.next = x; 
+			it.prev = x; 
+			x.next = it; 
+		}
+	}
+	private void add(T nuevo, int i) {
 		Nodo<T> x = new Nodo<T>(nuevo);
 		if (i>size()) {
 			i = size()-1;
@@ -76,9 +95,110 @@ public class Lista<T extends Comparable>
 		Nodo<T> it = root.prev; 
 		Nodo<T> itt = root; 
 		while (true) {
-			System.out.println(itt.dato.toString()); 
+			System.out.print(itt.dato.toString()+" "); 
 			if (it==itt) break; 
 			itt = itt.next;
 		}
+		System.out.println();
 	}	
+
+	public void desp(char c, int k) {
+		k = k%size();
+		Nodo<T> it = root; 
+		while (k-- > 0) {
+			if (c=='D') it = it.prev; 
+			else it = it.next; 
+		}
+
+		//Nodo<T> itt = root; 
+		//while (itt.next!=null) itt = itt.next; 
+		//itt.next = root; 
+		
+		root = it; 
+	}
+	public void del() {
+		Nodo<T> it = root; 
+		Nodo<T> itt = it.next; 
+		while (itt!=root) {
+			while (itt!=root && itt.dato.compareTo(it.dato) == 0) 
+				itt = itt.next; 
+			
+			if (itt==root) {
+				root.prev = it; 
+				it.next = root;
+				break; 
+			} else if (itt==it.next) {
+				it = itt; 
+				itt = itt.next; 
+				continue;
+			} 
+			else {
+				itt.prev = it; 
+				it.next = itt; 
+			}
+			it = itt; 
+			itt = itt.next; 
+		}
+		System.out.println("a");
+	}
 }
+
+
+
+/**
+ * 
+
+	1 1 2 3 5 6 6
+	del(); 
+	=> 1 2 3 5 6
+
+	l: 
+		ordenada => ordena? 
+		=> O(n*n*n): 
+			ordenamiento X
+			l.add(x)?? 
+			[x] que corresponda
+
+	2 casos: 
+	1 caso: es que exista un elemento proximo que sea 
+		nodo actual -> nodo diferente
+	2 caso: no existe un proximo elemento: 
+		nodo actual -> null; 
+	int size;
+
+	it = elemento A: 
+	itt = elemento A: it.value == itt.value && itt!=null) itt = itt.next; 
+
+	lista simple enlace: 
+		it.next = itt; 
+	lista doble enlace: 
+		if (it.next!=itt) : continue; 
+		else: 
+			it.next.prev = null; 
+			itt.prev.next = null;
+			it.next = itt; 
+			itt.prev = it; 	
+	LSEC: 
+	root; 
+		it = elemento A: 
+		itt = elemento A: it.value == itt.value && itt!=root) itt = itt.next; 
+		it.next = itt; //itt sea otro valor o caso contrario itt=root;
+
+	LDEC: 
+		it = elemento A: 
+		itt = elemento A: it.value == itt.value && itt!=root) itt = itt.next; 
+		
+		if (itt==it.next ||itt==root) {
+			root.prev.next = null; 
+			root.prev = it; 
+			it.next.prev = null; 
+			it.next = root; 
+		} else: 
+			itt.prev.next = null; 
+			itt.prev = it; 
+			it.next.prev = null; 
+			it.next = itt; 
+
+		<= seguir avanzando 
+		> agregalo aqui // antes del elemento
+ */
--- EstructurasDatos/Main.java
+++ EstructurasDatos/Main.java
@@ -2,12 +2,19 @@ public class Main {
 	public static void main(String[] args) {
 		Lista<Integer> l = new Lista(new Integer(4));
 		System.out.println(l.size());
-		l.add(new Integer(123), 20);
-		l.add(new Integer(332), 2122); 
-		l.add(new Integer(4354), 1);
-		l.imprimir();
-		l.erase(123);
+		l.add(new Integer(123));
+		l.add(new Integer(332)); 
+		l.add(new Integer(4354));
+		l.add(new Integer(123));
+		l.add(new Integer(345));
+		l.add(new Integer(4354));
+		l.add(new Integer(4354));
 		l.imprimir();
 		System.out.println(l.size()); 
+
+		l.del();
+		l.imprimir();
 	}
+	// 4, 123, 332, 4354
+	// 332, 4354, 4, 123
 }
