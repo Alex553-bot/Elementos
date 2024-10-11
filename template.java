757d9cdcc8031c44d9d9b73275a923372591814a
alexalvarez <alvarez.rojas.alexander.james5@gmail.com>
2024-10-11 16:23:57 -0400

    template 2.0

--- template.java
+++ template.java
@@ -1,9 +1,25 @@
-import java.util.Scanner; 
-public class Main 
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in); 
-		// aqui va tu solucion
-		sc.close();		
-	}
+import java.util.*;
+
+//Pueden llamar a su clase como quieran
+public class Main{
+    //Esta funcion es obligatoria, no cambiar, aqui es donde
+    //realizan reciben los datos de entrada e imprimen los resultados
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        //Aqui va tu solucion
+        sc.close();
+    }
+    
+    //Todos los metodos que vayan a hacer deben ser estaticos
+    static void f(){
+        
+    }
+    
+    static int f1(){
+        return 1;
+    }
+    
+    static boolean f2(){
+        return false;
+    }
 }
